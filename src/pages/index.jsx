ac33f3658315738173eafd3276972b762053cdee
Revku <revkuu@gmail.com>
2023-04-28 14:38:02 +0200

    Add SEO component and basic configuration

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { SEO } from 'components/Seo/Seo';
+
+const IndexPage = () => {
+	return <div>IndexPage</div>;
+};
+
+export default IndexPage;
+export const Head = () => <SEO title="Strona Główna" />;
