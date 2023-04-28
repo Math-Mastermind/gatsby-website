82f28aa45b54b75f355705c8dbb99ba2a0fa9a85
Revku <revkuu@gmail.com>
2023-04-28 17:01:32 +0200

    Add modes section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import 'styles/global.scss';
 import { SEO } from 'components/Seo/Seo';
+
 import Navigation from 'components/Navigation/Navigation';
 import Header from 'components/Header/Header';
+import ModesList from 'components/ModesList/ModesList';
 
 const IndexPage = () => {
 	return (
 		<div>
 			<Navigation />
 			<Header />
+			<ModesList />
 		</div>
 	);
 };
